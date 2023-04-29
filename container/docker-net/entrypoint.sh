fdec46e68bec36bf4cb3878b7db9ad59de040752
mbrav <mbrav@protonmail.com>
2023-04-30 00:00:46 +0300

    Add docker-net container for testing

--- container/docker-net/entrypoint.sh
+++ container/docker-net/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Default script command
 SCRIPT_COMMAND=run
@@ -13,18 +13,21 @@ count_arg=5
 help() {
     echo "ABOUT"
     echo "This is a entrypoint script for docker-net"
+    echo "Can run in an infinite loop and ping hosts"
     echo
     echo "SYNTAX"
     echo './entrypoint.sh [-h] [-i] [ARG] [run] [ping] [ARG]'
     echo
     echo "COMMANDS"
-    echo "run                         Run script as an infinite loop"
-    echo "ping                 [ARG]  Ping hosts passed as a space separated argument. Default: $docker_hosts"
+    echo "run                         Run script as a simple infinite loop"
+    echo "ping                 [ARG]  Ping hosts passed as a comma separated arguments. Default: $docker_hosts"
     echo
     echo "OPTIONS"
     echo "-h   --help                 Print this Help."
+    echo "-s   --hosts         [ARG]  Space seperated string list of hosts. Default: $docker_hosts"
     echo "-i   --interval      [ARG]  Specify interval in seconds. Default: $interval_arg"
     echo "-c   --count         [ARG]  Specify count argument. Default: $count_arg"
+    echo "-v   --verbose       [ARG]  Make ping verbose"
     echo
 }
 
@@ -40,6 +43,11 @@ else
                 shift # shift argument
                 exit 0
             ;;
+            --hosts|-s)
+                docker_hosts="$2"
+                shift # shift argument
+                shift # shift value
+            ;;
             --interval|-i)
                 interval_arg="$2"
                 shift # shift argument
@@ -50,6 +58,10 @@ else
                 shift # shift argument
                 shift # shift value
             ;;
+            --verbose|-v)
+                verbose=1
+                shift # shift argument
+            ;;
             run)
                 SCRIPT_COMMAND=run
                 shift # shift argument
@@ -58,7 +70,7 @@ else
                 SCRIPT_COMMAND=ping
                 shift # shift argument
                 # If arg value not empty, redefine value with passed argument 
-                [ -n "$1" ] && docker_hosts="$1" && shift  # shift value
+                # [ -n "$1" ] && docker_hosts="$1" && shift  # shift value
             ;;
             -*)
                 echo "Unknown option $1"
@@ -89,14 +101,17 @@ ping_cmd() {
     # Ping all specified hosts
     echo "Running ping command with interval $interval_arg seconds for hosts: $docker_hosts"
     count=0
+    
+    IFS=',' read -ra ping_hosts <<< "$docker_hosts"
+
     while true; do
         count=$(( count + 1 ))
         echo "Ping $count. Press [CTRL+C] to stop.."
 
-        for host in $docker_hosts; do
+        for host in "${ping_hosts[@]}"; do
             ping_result=$(ping -c "$count_arg" "$host")
-            echo "Pinged host $host $count_arg times:"
-            echo "$ping_result"
+            [[ -n $verbose ]] && echo "Pinged host $host $count_arg times:"
+            [[ -n $verbose ]] && echo "$ping_result"
         done
 
         sleep $interval_arg
